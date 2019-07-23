f9898543b73fb3a68547d375916043f4c5ed3c67
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-07-23 00:38:07 -0700

    started to work on the promo-sale-bottom area, still need to tweak the form

---
+++ resources/js/main.js
@@ -0,0 +1,7 @@
+const item1 = document.getElementById('item1');
+const item2 = document.getElementById('item2');
+const item3 = document.getElementById('item3');
+
+item1.innerHTML = '<span>$399.00</span>';
+item2.innerHTML = '<span>$299.00</span>';
+item3.innerHTML = '<span>$199.00</span>';
